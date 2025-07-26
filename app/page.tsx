dceb08c72c4804c036ab38c93c69b28f227166db
Birusha <birushandegeya@gmail.com>
2025-07-26 13:03:30 +0200

    feat(configuration): Add all files

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/common/mode-toggle";
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button>Birusha Ndegeya</Button>
+      <ModeToggle />
+    </div>
+  );
+}
